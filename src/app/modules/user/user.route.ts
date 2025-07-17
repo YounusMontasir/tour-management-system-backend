9d41fb18fd5faf9533c603a6a28cdd9369bf069e
unknown <ymontasir73@gmail.com>
2025-07-17 12:21:50 +0600

    part 1 and 2 complete

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,20 @@
+import { validateRequest } from './../../middlewares/validateRequest';
+import { UserControllers } from "./user.controller";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
+import { checkAuth } from '../../middlewares/checkAuth';
+import { Router } from 'express';
+import { Role } from './user.interface';
+
+
+
+const router = Router()
+
+
+
+router.post("/register", 
+    validateRequest(createUserZodSchema),
+    UserControllers.createUser)
+router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
+router.patch("/:id",validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
+
+export const UserRoutes = router
